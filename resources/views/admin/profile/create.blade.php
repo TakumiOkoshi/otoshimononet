397f4825ebf656b559fe16815baa2ea9343ed108
大越拓実 <yuzu.t.o7513@gmail.com>
2020-05-01 04:42:19 +0000

    view課題

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset= "utf-8">
+        <meta http-equiv= "X-UA-Compatible" content= "IE=edde">
+        <meta mame= "viewport" content= "width=device-width,initial-scale=1">
+        
+        <title>Kadai1</title>
+    </head>
+    <body>
+        <h1>Kadai1</h1>
+    </body>
+</html>
---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset= "utf-8">
+        <meta http-equiv= "X-UA-Compatible" content= "IE=edde">
+        <meta mame= "viewport" content= "width=device-width,initial-scale=1">
+        
+        <title>Kadai2</title>
+    </head>
+    <body>
+        <h1>Kadai2</h1>
+    </body>
+</html>
