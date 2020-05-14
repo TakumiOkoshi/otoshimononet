34f6ac99fe300ba416a8a8c7cd6e31e1170381fd
大越拓実 <yuzu.t.o7513@gmail.com>
2020-05-14 12:18:46 +0000

    14 投稿データを保存しよう課題

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -8,45 +8,47 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>My プロフィール</h2>
-              <form action="{{ action('Admin\ProfileController@create')}}"
-                 method="post"
-                 enctype="multipart/form-deta">
+                <form action="{{ action('Admin\ProfileController@create')}}"
+                method="post"
+                enctype="multipart/form-deta">
                     
-                  @if(count($errors) > 0)
-                    <ul>
-                        @foreach($errors->all() as $e)
-                        <li>{{ $e }}</li>
-                        @endforeach
-                    </ul>
+                  @if (count($errors) > 0)
+                        <ul>
+                           @foreach($errors->all() as $e)
+                           <li>{{ $e }}</li>
+                           @endforeach
+                        </ul>
                   @endif
                   
                   <div class="form-group row">
-                      <label class="col-md-5">氏名</label>
+                      <label class="col-md-5" for="title">氏名</label>
                       <div class="col-md-10">
                            <input type="text" class="form-control" placeholder="name">
                       </div>
                   </div>
                       
                   <div class="form-group row">
-                      <label class="col-md-5">性別</label>
+                      <label class="col-md-5" for="title">性別</label>
                       <div class="col-md-10">
                            <input type="text" class="form-control" placeholder="gender">
                       </div>
                   </div>
                   
                   <div class="form-group row">
-                      <label class="col-md-5">趣味</label>
+                      <label class="col-md-5" for="title">趣味</label>
                       <div class="col-md-10">
                           <input tyoe="text" class="form-control" placeholder="hobby">
                       </div>
                   </div>
                   
                   <div class="form-group row">
-                        <label class="col-md-5">自己紹介</label>
+                        <label class="col-md-5" for="body">自己紹介</label>
                         <div class="col-md-10">
                             <textarea class="form-control" name="body" rows="20" placeholder="introduction"></textarea>
                         </div>
                   </div>
+                  {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
                     
               </form>
             </div>
